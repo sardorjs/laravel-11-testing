79d799920f9779ca7c2f13cdadfc76bcd287f2a8
sardorjs <sardorjs17@gmail.com>
2024-10-11 18:29:17 +0500

    Laravel Testing 05/24: AAA Mantra: Arrange, Act, Assert

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -12,6 +12,17 @@ class ProductsTest extends TestCase
     //* before launching - change credentials in ./phpunit.xml file or .env.testing
     use RefreshDatabase; // always refresh migrate when launching tests
 
+    /**
+     * AAA "Arrange, Act, Assert" - Automated Testing - every function - must have these 3 things
+     * 1) Arrange - means prepare scenario - create all need credentials, info, data to work with
+     * 2) Act - calling some api, route, url, method - simulate user behavior
+     * 3) Assert - tests, assert status etc.
+     *
+     * * make test for each scenario
+     */
+
+
+
     public function test_products_page_contains_empty_table(): void
     {
         $response = $this->get('/products');
